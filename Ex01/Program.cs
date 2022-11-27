94b80c0e00254e3131e13e0132ec19e7675a5687
AlexBulavin <bulavin1970@icloud.com>
2022-11-27 15:39:09 +0300

    Commit 1

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,10 +1,13 @@
-Console.Writeline('Hi!');
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//ДЗ https://gbcdn.mrgcdn.ru/uploads/asset/4513851/attachment/9588b96fec66cd66b4c4efb73c1fe909.html
+//https://t.me/iksergeyru/21
 
 int number = 11; // Определили целое число
 
 int result = number * number;
 
-Console.Writeline(result);
+Console.WriteLine(result);
 
-Console.Writeline('End!');
+Console.WriteLine("End!");
 
